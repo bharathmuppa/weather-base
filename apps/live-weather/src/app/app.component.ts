7961a8f7415ca8a0cf777faf631d415756500a30
bharath <bharathmuppa@gmail.com>
2021-02-26 18:07:16 +0100

    initial commit

---
+++ apps/live-weather/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'weather-base-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'live-weather';
+}
