8e346b6442a36c852f41001f575558e59c117c40
bharath <bharathmuppa@gmail.com>
2021-03-05 16:47:13 +0100

    Fixed failing init tests

--- apps/live-weather/src/app/dashboard/dashboard.component.spec.ts
+++ apps/live-weather/src/app/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,6 @@
 // Copyright WeatherBase 2021
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
@@ -10,7 +11,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
+      declarations: [DashboardComponent],
+      imports: [HttpClientTestingModule]
     })
       .compileComponents();
   });
--- apps/live-weather/src/app/shared/air-pollution-card/air-pollution-card.component.spec.ts
+++ apps/live-weather/src/app/shared/air-pollution-card/air-pollution-card.component.spec.ts
@@ -1,16 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// Copyright WeatherBase 2021
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { createMock } from 'ts-auto-mock';
+
+import { of } from 'rxjs';
+
+import { OpenWeatherService } from '../../core/open-weather/open-weather.service';
 import { AirPollutionCardComponent } from './air-pollution-card.component';
+import { IAirPollution } from '../models/IAirPollution';
+import { AirQualityIndexPipe } from '../pipes/air-quality-index.pipe';
 
 describe('AirPollutionCardComponent', () => {
   let component: AirPollutionCardComponent;
   let fixture: ComponentFixture<AirPollutionCardComponent>;
 
   beforeEach(async () => {
+    const openWeatherServiceStub: Partial<OpenWeatherService> = {
+      getAirPollutionDetails: () => { return of(createMock<IAirPollution>()) }
+    }
+
     await TestBed.configureTestingModule({
-      declarations: [ AirPollutionCardComponent ]
+      declarations: [AirPollutionCardComponent, AirQualityIndexPipe],
+      imports: [MatToolbarModule, HttpClientTestingModule, RouterTestingModule.withRoutes([])],
+      providers: [{ provide: OpenWeatherService, useValue: openWeatherServiceStub }],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
